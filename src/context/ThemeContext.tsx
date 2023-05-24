4d76e17d7552da9545657dc263b99c1c758fe35f
SeniorToster <seraleks04@gmail.com>
2023-05-24 11:03:47 +0300

    Создан переключатель Темы

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, FC, useState } from "react"
+import { ChildrenProps } from "../types"
+
+enum Theme {
+    DARK = "dark",
+    LIGHT = "light",
+}
+export interface PropsTheme {
+    theme?: Theme
+    changeTheme?: () => void
+}
+
+export const themeContext = createContext<PropsTheme>({})
+
+export const ThemeContext: FC<ChildrenProps> = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>(Theme.DARK)
+
+    const changeTheme = () => {
+        const newTheme: Theme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+        setTheme(newTheme)
+    }
+
+    return <themeContext.Provider value={{ theme, changeTheme }}>{children}</themeContext.Provider>
+}
