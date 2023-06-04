effd4e9ee3b495daf4d4007ad65867622a3ebfa6
SeniorToster <seraleks04@gmail.com>
2023-06-04 19:26:28 +0300

    переписан рендер иконок в Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,50 +1,28 @@
 import style from "./Footer.module.scss"
-import { SiDiscord, SiVk, SiGithub } from "react-icons/si"
-import { HiMail } from "react-icons/hi"
 import { IconWrapper } from "../Ui/IconWrapper/IconWrapper.tsx"
-import { useState } from "react"
+import { useMessage } from "../../hooks/useMessage/useMessage.tsx"
+import { footer } from "../../config/links.json"
+import { icons } from "../../helpes/iconsArr.tsx"
 
 export const Footer = () => {
-    const [copyAccess, setCopyAccess] = useState(false)
-    const onCopyHandle = () => {
-        const copyText = "Госпожа  Тостерита#8888"
-        navigator.clipboard.readText().then((text) => {
-            if (copyText !== text) {
-                navigator.clipboard.writeText(copyText).then(() => {
-                    setCopyAccess((prevState) => !prevState)
-                    setTimeout(() => setCopyAccess((prevState) => !prevState), 2000)
-                })
-            }
-        })
-    }
+    const messageHandle = useMessage()
 
     return (
         <footer className={style.footer}>
             <div className={style.footer__wrapperIcons}>
-                <a href="mailto:seralek04@gmail.com">
-                    <IconWrapper title={"моя почта"}>
-                        <HiMail />
-                    </IconWrapper>
-                </a>
-                <div onClick={onCopyHandle}>
-                    {copyAccess && <div className={style.footer__copy}>Скопировано!</div>}
-                    <IconWrapper title={"мой профиль в Discord"}>
-                        <SiDiscord />
-                    </IconWrapper>
-                </div>
-                <a href="https://github.com/SeniorToster" target="_blank">
-                    <IconWrapper title={"мой профиль на GitHub"}>
-                        <SiGithub />
-                    </IconWrapper>
-                </a>
-
-                <a href="https://vk.com/id150518422" target="_blank">
-                    <IconWrapper title={"мой страничка VK"}>
-                        <SiVk />
-                    </IconWrapper>
-                </a>
+                {footer.map((item) => {
+                    const icon = icons.find((icon) => icon.name === item.name)
+                    return item.copy ? (
+                        <div key={item.name} onClick={() => messageHandle(item.link)}>
+                            <IconWrapper title={`профиль в ${item.name}`}>{icon?.iconDef}</IconWrapper>
+                        </div>
+                    ) : (
+                        <a key={item.name} href={item.link} target="_blank">
+                            <IconWrapper title={`профиль в ${item.name}`}>{icon?.iconDef}</IconWrapper>
+                        </a>
+                    )
+                })}
             </div>
-
             <div className={style.footer__corp}>
                 <p>
                     made by a <span>Toster</span>
