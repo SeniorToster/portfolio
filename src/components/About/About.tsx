3b93f62abc8fb77a02b2115edf1b3112bf54b3a5
SeniorToster <seraleks04@gmail.com>
2023-06-04 18:05:48 +0300

    добавлен постскриптум

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import { HashTags } from "../Ui/HashTags/HashTags.tsx"
 import styles from "./About.module.scss"
 import { Picture } from "../Ui/Picture/Picture.tsx"
 export const About = () => {
-    const { title, text, FrontEnd, BackEnd, DevOps, Other, BaseLang, img } = about
+    const { title, text, FrontEnd, BackEnd, DevOps, Other, BaseLang, img, ps, imgPs } = about
     return (
         <div className={styles.wrapper}>
             <h2>{title}</h2>
@@ -30,6 +30,9 @@ export const About = () => {
                 <h3>Остальное</h3>
                 <HashTags hashtags={Other} size="l" />
             </div>
+            <h3>Постскриптум</h3>
+            <p>{ps}</p>
+            <Picture img={imgPs} />
         </div>
     )
 }
